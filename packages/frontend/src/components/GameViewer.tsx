03692222875f9f78477001e2fd4dbde044524b96
Truong Nguyen <samuraitruong@hotmail.com>
2024-05-21 14:58:24 +1000

    feat: style updates

--- packages/frontend/src/components/GameViewer.tsx
+++ packages/frontend/src/components/GameViewer.tsx
@@ -7,6 +7,7 @@ import GameController from './Controller'
 import { useWindowSize } from '@uidotdev/usehooks';
 import { FaChessKing } from "react-icons/fa";
 import { FaRegChessKing } from "react-icons/fa6";
+import useKeyPress from '@/hooks/useKeyPress';
 
 interface GameViewerProps {
   data: GameEventResponse
@@ -16,8 +17,9 @@ interface GameViewerProps {
 const GameViewer = ({ data: { moves }, pair }: GameViewerProps) => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [fullscreen, setFullscreen] = useState(false);
-  const {height} = useWindowSize()
+  const { height } = useWindowSize()
   const [time, setTime] = useState({ black: 0, white: 0 });
+
 
   const handlePrevMove = () => {
     if (currentIndex > 0) {
@@ -30,6 +32,9 @@ const GameViewer = ({ data: { moves }, pair }: GameViewerProps) => {
       setCurrentIndex(currentIndex + 1);
     }
   };
+
+  useKeyPress('ArrowLeft', handlePrevMove)
+  useKeyPress('ArrowRight', handleNextMove)
   useEffect(() => {
     if (currentIndex % 2 === 0) {
       setTime({ ...time, white: moves[currentIndex].time })
@@ -40,25 +45,51 @@ const GameViewer = ({ data: { moves }, pair }: GameViewerProps) => {
   }, [currentIndex])
 
   const boardWidth = useMemo(() => {
-    if(!height) {
+    if (!height) {
       return 500
     }
-    if(!fullscreen) {
-      return height -160
+    if (!fullscreen) {
+      return height - 160
     }
-    return height-110
+    return height - 50
   }, [height, fullscreen])
   return (
     <div className={fullscreen ? 'fixed top-0 left-0 h-screen w-screen p-2 bg-slate-200 z-100 text-black' : ''}>
-      <div className={fullscreen?'flex justify-center': 'flex justify-center'}>
-        <div>
-          <div className="flex justify-between">
-            <div className='font-bold'>
-              <FaChessKing className='inline' />{pair.black}
+      <div className={fullscreen ? 'flex justify-center' : 'flex justify-center'}>
+        {fullscreen && <div className='flex p-5 items-center justify-center align-middle h-100 flex-col mr-10'>
+          <div className="flex items-center justify-center text-4xl">
+            <div className="flex justify-between flex-col justify-center">
+              <div className='font-bold mb-5'>
+                <FaChessKing className='inline' />{pair.black}
+              </div>
+              <div className='flex justify-around'>
+                <Clock time={time.black} ></Clock>
+              </div>
             </div>
-            <Clock time={time.black} ></Clock>
           </div>
+          <div className="flex items-center justify-center mt-20 mb-20 size-10 text-6xl font-semibold">vs</div>
+          <div className="flex items-center justify-center text-4xl">
 
+            <div className="flex justify-between flex-col">
+              <div className='font-bold mb-5'>
+                <FaRegChessKing className='inline' />{pair.white}
+              </div>
+              <div className='flex justify-around'>
+                <Clock time={time.white} ></Clock>
+              </div>
+            </div>
+
+          </div>
+        </div>}
+        <div>
+          {!fullscreen &&
+            <div className="flex justify-between">
+              <div className='font-bold'>
+                <FaChessKing className='inline' />{pair.black}
+              </div>
+              <Clock time={time.black} ></Clock>
+            </div>
+          }
           <Chessboard
             boardWidth={boardWidth}
             position={moves[currentIndex].fen}
@@ -68,13 +99,13 @@ const GameViewer = ({ data: { moves }, pair }: GameViewerProps) => {
             customArrows={[moves[currentIndex].arrow] as any}
 
           />
-
-          <div className="flex justify-between">
-          <div className='font-bold'>
-              <FaRegChessKing className='inline'/>{pair.white}
-            </div>
-            <Clock time={time.white} ></Clock>
-          </div>
+          {!fullscreen &&
+            <div className="flex justify-between">
+              <div className='font-bold'>
+                <FaRegChessKing className='inline' />{pair.white}
+              </div>
+              <Clock time={time.white} ></Clock>
+            </div>}
 
           <GameController handleNextMove={handleNextMove}
             handlePrevMove={handlePrevMove}
---
+++ packages/frontend/src/hooks/useKeyPress.ts
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react';
+
+const useKeyPress = (targetKey: string, callback: () => void) => {
+    const [keyPressed, setKeyPressed] = useState(false);
+
+    const downHandler = ({ key }: any) => {
+        console.log('key', key)
+        if (key === targetKey) {
+            setKeyPressed(true);
+            callback()
+        }
+    };
+
+    // const upHandler = ({ key }: any) => {
+    //     if (key === targetKey) {
+    //         setKeyPressed(false);
+    //     }
+    // };
+
+    useEffect(() => {
+        window.addEventListener('keydown', downHandler);
+        //window.addEventListener('keyup', upHandler);
+
+        // Cleanup function
+        return () => {
+            window.removeEventListener('keydown', downHandler);
+            // window.removeEventListener('keyup', upHandler);
+        };
+    }, [targetKey, callback]); // Re-run effect if targetKey changes
+
+    return keyPressed;
+};
+
+export default useKeyPress;
