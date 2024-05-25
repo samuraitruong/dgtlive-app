a36e2eb2f6cc3c4f926f661cf624793ebc1c308b
Truong Nguyen <samuraitruong@hotmail.com>
2024-05-25 13:59:09 +1000

    update move list layout

--- packages/frontend/src/components/MoveList.tsx
+++ packages/frontend/src/components/MoveList.tsx
@@ -20,11 +20,18 @@ const chunkArray = (arr: any[], size: number) => {
 function MoveList({ moves, onSelect, selectedIndex, maxHeight, delayedMoves }: MoveListProps) {
     const pairs = chunkArray(moves, 2)
     return (
-        <div className="flex flex-col w-[300px] pt-3 pl-5  overflow-y-auto relative" style={{ height: maxHeight + 20 }}>
+        <div className="flex flex-col w-[300px] ml-5 pt-3 pl-5  overflow-y-auto relative border border-solid border-red" style={{ height: maxHeight + 20 }}>
+            <div className="flex w-full justify bg-blue-500 p-1 text-white">
+                <div className='w-2/12'>No.</div>
+                <div className="mr-4 w-5/12 cursor-pointer hover:font-bold">White</div>
+                <div className="mr-4 w-5/12 cursor-pointer hover:font-bold">Black</div>
+            </div>
+
             {pairs.map((t: Move[], index: number) => (
-                <div key={index} className="flex w-full justify border-b-indigo-400">
-                    <div className={"mr-4 w-1/2 cursor-pointer hover:font-bold " + (selectedIndex == index * 2 ? 'font-bold' : '')} onClick={() => onSelect(index * 2)}>{index + 1}. {t[0].san}</div>
-                    <div className={"mr-4 w-1/2 cursor-pointer hover:font-bold " + (selectedIndex == index * 2 + 1 ? 'font-bold' : '')} onClick={() => onSelect(index * 2 + 1)}>{t[1]?.san}</div>
+                <div key={index} className="flex w-full justify border-b-indigo-400 p-1">
+                    <div className='w-2/12'>{index + 1}.</div>
+                    <div className={"mr-4 w-5/12 cursor-pointer hover:font-bold " + (selectedIndex == index * 2 ? 'font-bold' : '')} onClick={() => onSelect(index * 2)}> {t[0].san}</div>
+                    <div className={"mr-4 w-5/12 cursor-pointer hover:font-bold " + (selectedIndex == index * 2 + 1 ? 'font-bold' : '')} onClick={() => onSelect(index * 2 + 1)}>{t[1]?.san}</div>
                 </div>
             ))
             }
